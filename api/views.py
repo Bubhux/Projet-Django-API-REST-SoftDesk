5ed05de9a6cceb53c36c3324a966710bef74424e
Bubhux <bubhuxpaindepice@gmail.com>
2023-08-03 05:59:30 +0200

    Mise à jour des apps api et user

--- api/views.py
+++ api/views.py
@@ -1,57 +1,38 @@
 from rest_framework import status
+from rest_framework.generics import get_object_or_404, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets
 
 from api.models import Project, Issue, Contributor, Comment
 from api.permissions import ProjectPermissions, ContributorPermissions, IssuePermissions, CommentPermissions
-from api.serializers import (
-    ProjectListSerializer,
-    ProjectDetailSerializer,
-    ContributorListSerializer,
-    ContributorDetailSerializer,
-    IssueListSerializer, IssueDetailSerializer,
-    CommentListSerializer,
-    CommentDetailSerializer
-)
+from api.serializers import ProjectSerializer, IssueSerializer, CommentSerializer, ContributorSerializer
 from user.serializers import UserSignupSerializer
 
 
-class MultipleSerializerMixin:
-    """Mixin pour utiliser plusieurs classes de sérialiseur dans une vue."""
-
-    # Par défaut, la variable detail_serializer_class est définie sur None.
-    # Ce qui signifie qu'il n'y a pas de sérialiseur spécifique défini pour les actions de type "retrieve", "create", "update" et "destroy".
-    detail_serializer_class = None
-
-    def get_serializer_class(self):
-        if (self.action == 'retrieve' or
-                self.action == 'create' or
-                self.action == 'update' or
-                self.action == 'destroy') and self.detail_serializer_class is not None:
-            return self.detail_serializer_class
-        return super().get_serializer_class()
-
-
-class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
-    """ViewSet d'API pour gérer les opérations CRUD sur les objets Project."""
-
+class ProjectListViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
-    serializer_class = ProjectListSerializer
-    detail_serializer_class = ProjectDetailSerializer
+    serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated, ProjectPermissions]
 
     def perform_create(self, serializer):
-        """Sauvegarde l'objet Project avec l'utilisateur actuellement connecté en tant qu'auteur."""
         serializer.save(author=self.request.user)
 
     def get_queryset(self):
-        """Renvoie les projets auxquels l'utilisateur actuel est contributeur."""
         return self.queryset.filter(contributors__user=self.request.user)
 
+
+class ProjectDetailViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, ProjectPermissions]
+
+    def get_object(self):
+        project_pk = self.kwargs.get('pk')
+        return get_object_or_404(Project, id=project_pk)
+
     def put(self, request, pk=None):
-        """Met à jour l'objet Project spécifié par la clé primaire passée dans l'URL."""
         project = self.get_object()
         data = request.data.copy()
         data['author'] = project.author.id
@@ -62,31 +43,24 @@ class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk=None):
-        """Supprime l'objet Project spécifié par la clé primaire passée dans l'URL."""
         project = self.get_object()
         project.delete()
         return Response('Project successfully deleted.', status=status.HTTP_204_NO_CONTENT)
 
 
-class UserContributorsViewSet(MultipleSerializerMixin, ModelViewSet):
-    """ViewSet d'API pour gérer les contributeurs d'un objet Project spécifique."""
-
-    serializer_class = ContributorListSerializer
-    detail_serializer_class = ContributorDetailSerializer
+class UserContributorsListViewSet(viewsets.ModelViewSet):
+    serializer_class = ContributorSerializer
     permission_classes = [IsAuthenticated, ContributorPermissions]
 
     def get_project(self, project_pk):
-        """Récupère l'objet Project spécifié par la clé primaire passée dans l'URL."""
         return get_object_or_404(Project, id=project_pk)
 
     def get_queryset(self):
-        """Renvoie les contributeurs associés à l'objet Project spécifié dans l'URL."""
         project_pk = self.kwargs.get('project_pk')
         project = self.get_project(project_pk)
         return Contributor.objects.filter(project=project)
 
     def create(self, request, project_pk):
-        """Ajoute un nouveau contributeur à l'objet Project spécifié dans l'URL."""
         project = self.get_project(project_pk)
         data = request.data.copy()
         data['project'] = project.id
@@ -105,8 +79,21 @@ class UserContributorsViewSet(MultipleSerializerMixin, ModelViewSet):
             except User.DoesNotExist:
                 return Response('This user does not exist.', status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserContributorsDetailViewSet(viewsets.ModelViewSet):
+    serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated, ContributorPermissions]
+
+    def get_project(self, project_pk):
+        return get_object_or_404(Project, id=project_pk)
+
+    def get_object(self):
+        project_pk = self.kwargs.get('project_pk')
+        project = self.get_project(project_pk)
+        contributor_pk = self.kwargs.get('contributor_pk')
+        return get_object_or_404(Contributor, id=contributor_pk, project=project)
+
     def destroy(self, request, project_pk, contributor_pk):
-        """Supprime le contributeur spécifié de l'objet Project spécifié."""
         project = self.get_project(project_pk)
         contributor = self.get_object()
         if contributor.role == 'AUTHOR':
@@ -116,76 +103,123 @@ class UserContributorsViewSet(MultipleSerializerMixin, ModelViewSet):
             return Response('Contributor successfully deleted.', status=status.HTTP_204_NO_CONTENT)
 
 
-class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
-    """ViewSet d'API pour gérer les issues d'un objet Project spécifique."""
-
-    serializer_class = IssueListSerializer
-    detail_serializer_class = IssueDetailSerializer
+class IssueListViewSet(viewsets.ModelViewSet):
+    serializer_class = IssueSerializer
     permission_classes = [IsAuthenticated, IssuePermissions]
 
     def get_project(self, project_pk):
-        """Récupère l'objet Project spécifié par la clé primaire passée dans l'URL."""
         return get_object_or_404(Project, id=project_pk)
 
     def get_queryset(self):
-        """Renvoie les issues associées à l'objet Project spécifié dans l'URL."""
         project_pk = self.kwargs.get('project_pk')
         project = self.get_project(project_pk)
         return Issue.objects.filter(project=project)
 
     def perform_create(self, serializer):
-        """Sauvegarde un nouvel objet Issue associé à l'objet Project spécifié et à l'auteur actuellement connecté."""
         project_pk = self.kwargs.get('project_pk')
         project = self.get_project(project_pk)
         serializer.save(project=project, author=self.request.user)
 
     def create(self, request, project_pk):
-        """Crée un nouvel objet Issue associé à l'objet Project spécifié."""
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, project_pk, pk):
-        """Supprime l'objet Issue spécifié associé à l'objet Project spécifié."""
         project = self.get_project(project_pk)
         issue = self.get_object()
         issue.delete()
         return Response('Issue successfully deleted.', status=status.HTTP_204_NO_CONTENT)
 
 
-class CommentViewSet(MultipleSerializerMixin, ModelViewSet):
-    """ViewSet d'API pour gérer les commentaires d'un issue spécifique d'un objet Project."""
+class IssueDetailViewSet(viewsets.ModelViewSet):
+    serializer_class = IssueSerializer
+    permission_classes = [IsAuthenticated, IssuePermissions]
 
-    serializer_class = CommentListSerializer
-    detail_serializer_class = CommentDetailSerializer
+    def get_project(self, project_pk):
+        return get_object_or_404(Project, id=project_pk)
+
+    def get_object(self):
+        project_pk = self.kwargs.get('project_pk')
+        issue_pk = self.kwargs.get('issue_pk')
+        project = self.get_project(project_pk)
+        return get_object_or_404(Issue, id=issue_pk, project=project)
+
+    def perform_update(self, serializer):
+        issue = self.get_object()
+        serializer.save(project=issue.project, author=issue.author)
+
+    def update(self, request, project_pk, issue_pk):
+        issue = self.get_object()
+        serializer = self.serializer_class(issue, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, project_pk, issue_pk):
+        issue = self.get_object()
+        issue.delete()
+        return Response('Issue successfully deleted.', status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentListViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, CommentPermissions]
 
     def get_project_and_issue(self, project_pk, issue_pk):
-        """Récupère l'objet Project et l'objet Issue spécifiés par les clés primaires passées dans l'URL."""
         project = get_object_or_404(Project, id=project_pk)
         issue = get_object_or_404(Issue, id=issue_pk)
         return project, issue
 
     def get_queryset(self):
-        """Renvoie les commentaires associés à l'objet Issue spécifié dans l'URL."""
         project_pk = self.kwargs.get('project_pk')
         issue_pk = self.kwargs.get('issue_pk')
         project, issue = self.get_project_and_issue(project_pk, issue_pk)
         return Comment.objects.filter(issue=issue)
 
     def perform_create(self, serializer):
-        """Sauvegarde un nouveau commentaire associé à l'objet Issue spécifié et à l'auteur actuellement connecté."""
         project_pk = self.kwargs.get('project_pk')
         issue_pk = self.kwargs.get('issue_pk')
         project, issue = self.get_project_and_issue(project_pk, issue_pk)
         serializer.save(issue=issue, author=self.request.user)
 
     def perform_update(self, serializer):
-        """Met à jour l'objet Comment avec les nouvelles données en conservant l'issue et l'auteur d'origine."""
         comment = self.get_object()
         serializer.save(issue=comment.issue, author=comment.author)
 
     def perform_destroy(self, instance):
-        """Supprime l'objet Comment spécifié."""
         instance.delete()
+
+
+class CommentDetailViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, CommentPermissions]
+
+    def get_project_and_issue(self, project_pk, issue_pk):
+        project = get_object_or_404(Project, id=project_pk)
+        issue = get_object_or_404(Issue, id=issue_pk)
+        return project, issue
+
+    def get_object(self):
+        project_pk = self.kwargs.get('project_pk')
+        issue_pk = self.kwargs.get('issue_pk')
+        comment_pk = self.kwargs.get('comment_pk')
+        project, issue = self.get_project_and_issue(project_pk, issue_pk)
+        return get_object_or_404(Comment, id=comment_pk, issue=issue)
+
+    def perform_update(self, serializer):
+        comment = self.get_object()
+        serializer.save(issue=comment.issue, author=comment.author)
+
+    def update(self, request, project_pk, issue_pk, comment_pk):
+        comment = self.get_object()
+        serializer = self.serializer_class(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, project_pk, issue_pk, comment_pk):
+        comment = self.get_object()
+        comment.delete()
+        return Response('Comment successfully deleted.', status=status.HTTP_204_NO_CONTENT)
