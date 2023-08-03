745cefd7d4004947648bbe8064ac84e95fb2bd63
Bubhux <bubhuxpaindepice@gmail.com>
2023-08-03 05:53:01 +0200

    Mise à jour apps user

--- user/admin.py
+++ user/admin.py
@@ -6,5 +6,6 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = ['username', 'email', 'is_staff', 'is_active', 'age']
     list_filter = ('username', 'is_superuser', 'is_staff', 'is_active')
+    readonly_fields = ['date_joined']
 
 admin.site.register(User, UserAdmin)
